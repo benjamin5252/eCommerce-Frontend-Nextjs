0f6bdd86d175f16184d1c81da37a6332d1645e43
David <k0960355356@gmail.com>
2022-07-29 16:40:16 +0800

    <feat>(<eComFront>)<cart page>

--- pages/cart/[[...id]].js
+++ pages/cart/[[...id]].js
@@ -5,12 +5,21 @@ import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../../components/Message'
 import { addToCart, setCartItemsFromStorage } from '../../store/cart'
 import { useRouter } from 'next/router'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as fas from "@fortawesome/free-solid-svg-icons";
+import styles from './CartPage.module.scss'
 
 
 const CartPage = () => {
   const dispatch = useDispatch();
   const router = useRouter();
 
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
+
+
+  let productId = null
+  let qty = null
   let routerIsReady = false;
 
   useEffect(()=>{
@@ -18,14 +27,64 @@ const CartPage = () => {
   }, [])
 
   useEffect(()=>{
+
     if(!routerIsReady && router.isReady){
-      console.log(router.query)
+      // console.log(router.query)
+      // {qty: '0', id: Array(1)} id: ['62dfb0893ff0886be9c40774']qty: "0"[[Prototype]]: Object
+      if(router.query.id && router.query.id[0]){
+        productId = router.query.id[0]
+      }
+      if(router.query.qty){
+        qty = router.query.qty
+      }
+      if( productId && qty){
+        dispatch(addToCart(productId, qty))
+      }
+      
       routerIsReady = true;
     }
   },[router.isReady])
 
   return (
-    <div>CartPage</div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        { cartItems.length === 0 ? 
+        (<Message>Your cart is empty <Link href='/'>Go Back</Link></Message>) 
+        : 
+        (<ListGroup variant='flush'>
+          {cartItems.map(item => (
+            <ListGroup.Item key={item.product}>
+              <Row>
+                <Col className={styles.col} md={2}>
+                  <Image src={item.image} alt={item.name} fluid rounded/>
+                </Col>
+                <Col className={styles.col} md={3}>
+                  <Link href={`/product/${item.product}`}>{item.name}</Link>
+                </Col>
+                <Col className={styles.col} md={2}>${item.price}</Col>
+                <Col className={styles.col} md={2}>
+                  <Form.Control as='select' value={item.qty} onChange={(e)=> dispatch((addToCart(item.product, Number(e.target.value))))}>
+                    {[...Array(item.countInStock).keys()].map( i => (<option key={i + 1} value={i + 1} >{ i + 1 }</option>))}
+                  </Form.Control>
+                </Col>
+                <Col className={styles.col} md={2}>
+                  <Button type='button' variant='light' onClick={()=>{removeFromCartHandler(item.product)}}>
+                    <FontAwesomeIcon icon={fas.faTrash} />
+                  </Button>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>) }
+      </Col>
+      <Col md={2}>
+
+      </Col>
+      <Col md={2}>
+
+      </Col>
+    </Row>
   )
 }
 
